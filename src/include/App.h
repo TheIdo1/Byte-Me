6843cef70f3eb3edb7bc8c250daf879a2de4d8f3
Tamir Avraham <tamir.792001@gmail.com>
2026-04-24 16:44:42 +0300

    UPDATED: only infrastructure template - removed any logic for now

--- src/include/App.h
+++ src/include/App.h
@@ -7,22 +7,10 @@ class App {
     public:
         //constructor
         App () {}
-        void run(){
-            // implement things
-        }
+        
+        void run(){}
 
-        //TODO - clean the App from validation of command - create a class for command validations
-        // version 1: string as argument - used for test purposes
-        bool isValidCommand(const std::string& input);
 
-        // version 2: string and vector as arguments - used for real purposes
-        bool isValidCommand(const std::string& input, const std::vector<std::string>& tokens);
-        private:
-        // helpers
-        //check if string contains only numeric chars
-        bool isNumeric(const std::string& s);
-        // the method gets input and empty tokens string vector and extracts the entries of input into the vector
-        void inputParseIntoTokens(const std::string& input, std::vector<std::string>& tokens);
 };
 
 #endif
