36c63413a816b94b45e3dae962d06b631d5c9b83
Yuval Hatuka <yuvalhatuka27@gmail.com>
2026-05-03 20:34:04 +0300

    changed tests according to changes

--- tests/add_command_tests.cpp
+++ tests/add_command_tests.cpp
@@ -16,40 +16,95 @@ protected:
 };
 
 TEST_F(AddCommandTest, ReturnsFalseWhenTooFewArguments) {
-    AddCommand command;
+    AddCommand command(&UserManager::getInstance(), &ProductManager::getInstance());
     vector<string> args = {"1"};
     EXPECT_FALSE(command.validate(args));
 }
 
-TEST_F(AddCommandTest, ReturnsFalseWhenUserIdIsInvalid) {
-    AddCommand command;
-    vector<string> args = {"abc", "100"};
-    EXPECT_FALSE(command.validate(args));
-}
-
-TEST_F(AddCommandTest, ReturnsFalseWhenUserDoesNotExist) {
-    AddCommand command;
+TEST_F(AddCommandTest, ValidateReturnsTrueForValidArgs) {
+    AddCommand command(&UserManager::getInstance(), &ProductManager::getInstance());
     vector<string> args = {"1", "100"};
-    EXPECT_FALSE(command.validate(args));
+    EXPECT_TRUE(command.validate(args));
 }
 
-TEST_F(AddCommandTest, ReturnsFalseWhenProductIdIsInvalid) {
+TEST_F(AddCommandTest, ValidateReturnsTrueEvenWhenUserDoesNotExist) {
+    AddCommand command(&UserManager::getInstance(), &ProductManager::getInstance());
+    vector<string> args = {"999", "100"};
+    EXPECT_TRUE(command.validate(args)); // validate doesn't check existence
+}
+
+TEST_F(AddCommandTest, ExecuteThrowsWhenUserIdIsInvalid) {
+    AddCommand command(&UserManager::getInstance(), &ProductManager::getInstance());
+    vector<string> args = {"abc", "100"};
+    EXPECT_TRUE(command.validate(args)); // validate passes
+    EXPECT_THROW(command.execute(args), invalid_argument); // stoi fails
+}
+
+TEST_F(AddCommandTest, ExecuteThrowsWhenProductIdIsInvalid) {
     UserManager::getInstance().addUser(1, "Alice");
-    AddCommand command;
+    AddCommand command(&UserManager::getInstance(), &ProductManager::getInstance());
     vector<string> args = {"1", "xyz"};
-    EXPECT_FALSE(command.validate(args));
+    EXPECT_TRUE(command.validate(args)); // validate passes
+    EXPECT_THROW(command.execute(args), invalid_argument); // stoi fails
 }
 
-TEST_F(AddCommandTest, ReturnsFalseWhenProductDoesNotExist) {
-    UserManager::getInstance().addUser(2, "Bob");
-    AddCommand command;
-    vector<string> args = {"2", "999"};
-    EXPECT_FALSE(command.validate(args));
+TEST_F(AddCommandTest, ExecuteCreatesUserIfNotExists) {
+    AddCommand command(&UserManager::getInstance(), &ProductManager::getInstance());
+    vector<string> args = {"10", "100"};
+    EXPECT_TRUE(command.validate(args));
+    command.execute(args);
+    User* user = UserManager::getInstance().getUser(10);
+    ASSERT_NE(user, nullptr);
+    EXPECT_EQ(user->getId(), 10);
+    EXPECT_EQ(user->getName(), "User-10");
+    EXPECT_EQ(user->getProductsWatched().size(), 1u);
+    EXPECT_EQ(user->getProductsWatched()[0].getId(), 100);
+    delete user;
+}
+
+TEST_F(AddCommandTest, ExecuteCreatesProductIfNotExists) {
+    UserManager::getInstance().addUser(11, "Dave");
+    AddCommand command(&UserManager::getInstance(), &ProductManager::getInstance());
+    vector<string> args = {"11", "200"};
+    EXPECT_TRUE(command.validate(args));
+    command.execute(args);
+    Product* product = ProductManager::getInstance().getProduct(200);
+    ASSERT_NE(product, nullptr);
+    EXPECT_EQ(product->getId(), 200);
+    EXPECT_EQ(product->getName(), "Product-200");
+    // Price is random, just check it's set
+    EXPECT_GE(product->getPrice(), 0.0);
+    EXPECT_LE(product->getPrice(), 999.0);
+    User* user = UserManager::getInstance().getUser(11);
+    ASSERT_NE(user, nullptr);
+    EXPECT_EQ(user->getProductsWatched().size(), 1u);
+    EXPECT_EQ(user->getProductsWatched()[0].getId(), 200);
+    delete user;
+    delete product;
+}
+
+TEST_F(AddCommandTest, ExecuteCreatesBothUserAndProductIfNotExist) {
+    AddCommand command(&UserManager::getInstance(), &ProductManager::getInstance());
+    vector<string> args = {"12", "201"};
+    EXPECT_TRUE(command.validate(args));
+    command.execute(args);
+    User* user = UserManager::getInstance().getUser(12);
+    ASSERT_NE(user, nullptr);
+    EXPECT_EQ(user->getId(), 12);
+    EXPECT_EQ(user->getName(), "User-12");
+    Product* product = ProductManager::getInstance().getProduct(201);
+    ASSERT_NE(product, nullptr);
+    EXPECT_EQ(product->getId(), 201);
+    EXPECT_EQ(product->getName(), "Product-201");
+    EXPECT_EQ(user->getProductsWatched().size(), 1u);
+    EXPECT_EQ(user->getProductsWatched()[0].getId(), 201);
+    delete user;
+    delete product;
 }
 
 TEST_F(AddCommandTest, ExecuteAddsProductsToUser) {
     UserManager::getInstance().addUser(3, "Charlie");
-    AddCommand command;
+    AddCommand command(&UserManager::getInstance(), &ProductManager::getInstance());
     vector<string> args = {"3", "100", "101"};
     EXPECT_TRUE(command.validate(args));
     command.execute(args);
@@ -61,6 +116,6 @@ TEST_F(AddCommandTest, ExecuteAddsProductsToUser) {
 }
 
 TEST_F(AddCommandTest, GetDescriptionReturnsExpectedString) {
-    AddCommand command;
+    AddCommand command(&UserManager::getInstance(), &ProductManager::getInstance());
     EXPECT_EQ(command.getDescription(), "add [userId] [productId1] [productId2] ...");
 }
