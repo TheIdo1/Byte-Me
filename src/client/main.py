470364416cf837b4196390ae51fd203cb951f2e9
The Ido <idoitzhak01@gmail.com>
2026-05-17 21:32:33 +0300

    update readme

--- src/client/main.py
+++ src/client/main.py
@@ -3,7 +3,7 @@ import sys
 
 def main():
     if len(sys.argv) != 3:
-        # print("Usage: python main.py <IP> <PORT>")
+        print("Usage: python main.py <IP> <PORT>")
         return
 
     ip = sys.argv[1]
@@ -13,13 +13,12 @@ def main():
         port = int(sys.argv[2])
     except ValueError:
         # contract said do not print errors GOD KNOWS WHY 
-        # print("Error: Port must be an integer.")
+        print("Error: Port must be an integer.")
         return
     
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         soc.connect((ip,port))
-        
         #infinite loop of input and output through socket and cli
         while True:
             user_input = input()
