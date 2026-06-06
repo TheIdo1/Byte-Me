ecba14f840a946bbef4f6c2e037386556b6385e7
Tamir Avraham <tamir.792001@gmail.com>
2026-06-06 20:18:14 +0300

    restircting endpoints

--- src/webServer/routes/restaurants.route.js
+++ src/webServer/routes/restaurants.route.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const restaurantsController = require('../controllers/restaurants.controller');
 const { validateCreateRestaurant, validateRestaurantUpdate } = require('../middleware/validators/restaurantValidator');
 const { validateAddressMiddleware, validateAddressOptional }= require('../middleware/validators/addressValidator')
+const { requireAuth } = require('../middleware/auth'); // Import the authentication middleware
 
 
 //route products to products.route
@@ -14,12 +15,14 @@ router.use('/:rId/products', productsRouter);
 
 router.route('/')
     .get(restaurantsController.getAllRestaurants)
-    .post(validateCreateRestaurant, validateAddressMiddleware, restaurantsController.createRestaurant);
+    // Add requireAuth before the controller to protect this endpoint
+    .post(requireAuth,validateCreateRestaurant, validateAddressMiddleware, restaurantsController.createRestaurant);
 
 
 router.route('/:rId')
     .get(restaurantsController.getRestaurantById)
-    .patch(validateRestaurantUpdate, validateAddressOptional, restaurantsController.updateRestaurant)
-    .delete(restaurantsController.deleteRestaurant);
+    // Add requireAuth to protect updates and deletions
+    .patch(requireAuth,validateRestaurantUpdate, validateAddressOptional, restaurantsController.updateRestaurant)
+    .delete(requireAuth,restaurantsController.deleteRestaurant);
 
 module.exports = router;
