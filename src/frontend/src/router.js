7f860e6670ebb362eb548d5bf7329bfec3c7a016
Tamir Avraham <tamir.792001@gmail.com>
2026-06-09 16:47:07 +0300

    updated router in react

--- src/frontend/src/router.js
+++ src/frontend/src/router.js
@@ -1,11 +1,16 @@
 // Central route registry. Add all new pages here as <Route> entries.
 import { Routes, Route } from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage'; 
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      
+      {/* Temporary home page route to ensure the redirect works smoothly after a successful login */}
+      <Route path="/" element={<h1 style={{ textAlign: 'center', marginTop: '50px' }}>Welcome to Byte Me</h1>} />
     </Routes>
   );
 }
