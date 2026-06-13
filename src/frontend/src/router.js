4c12e256408445c9bb8004081ef7f85c5afb0e4f
The Ido <idoitzhak01@gmail.com>
2026-06-13 22:42:17 +0300

    add restaurant route

--- src/frontend/src/router.js
+++ src/frontend/src/router.js
@@ -2,12 +2,16 @@
 import { Routes, Route } from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage'; 
+import RestaurantPage from './pages/RestaurantPage';
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
+
+      {/* dynamic route for individual restaurant pages */}
+      <Route path="/restaurants/:restaurantId" element= {<RestaurantPage />}/>
       
       {/* Temporary home page route to ensure the redirect works smoothly after a successful login */}
       <Route path="/" element={<h1 style={{ textAlign: 'center', marginTop: '50px' }}>Welcome to Byte Me</h1>} />
